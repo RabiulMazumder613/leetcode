73a09dd0650c8fefadc24bf2e0845a7cbcd3df16
Rabiul Mazumder <60079850+RabiulMazumder613@users.noreply.github.com>
2021-08-03 16:40:32 -0400

    add 217

--- 217 Contains Duplicate.py
+++ 217 Contains Duplicate.py
@@ -12,3 +12,12 @@ class Solution(object):
             else:
                 return True
         return False
+    
+# Fastest Solution:
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
