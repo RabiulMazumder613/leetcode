7369b3158f5228bc862ceae3caf8457b74583ed2
Rabiul Mazumder <60079850+RabiulMazumder613@users.noreply.github.com>
2021-08-03 18:31:11 -0400

    Create 268. Missing Number.py

---
+++ 268. Missing Number.py
@@ -0,0 +1,20 @@
+# My Brute Force Solution: (Very Slow)
+# Even with using nums.sort() still very slow
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for n in range(len(nums) + 1):
+            if n not in nums:
+                return n
+              
+# HashSet (Faster) Time Complexity O(n) | Space Complexity: O(n)
+# By inserting each element of nums into a set
+# it allows us to later query for containment in O(1) time.
+class Solution:
+    def missingNumber(self, nums):
+        num_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
+[![Missing Number - LeetCode 268](https://img.youtube.com/vi/i0WwsQYrMAM/0.jpg)](https://www.youtube.com/watch?v=i0WwsQYrMAM "Missing Number - LeetCode 268")
+
