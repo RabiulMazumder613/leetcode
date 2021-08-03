8267161b6bafef4335152bccd8e40aa54deda462
Rabiul Mazumder <60079850+RabiulMazumder613@users.noreply.github.com>
2021-08-03 16:37:07 -0400

    Create 217 Contains Duplicate.py

---
+++ 217 Contains Duplicate.py
@@ -0,0 +1,14 @@
+# My Initial Solution: (Very Slow)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hold = []
+        for i in nums:
+            if i not in hold:
+                hold.append(i)
+            else:
+                return True
+        return False
